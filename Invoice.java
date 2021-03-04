aade2852414f9676a849129659af6b52774466f0
Dio Fajrie <diofajriefadlullah@gmail.com>
2021-03-04 17:09:58 +0700

    modul 1

---
+++ Invoice.java
@@ -0,0 +1,53 @@
+
+/**
+ * Write a description of class Invoice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Invoice
+{
+    // instance variables - replace the example below with your own
+    private int id;
+    private int idJob;
+    private String date;
+    private int totalFee;
+    /**
+     * Constructor for objects of class Invoice
+     */
+    public Invoice()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getId()
+    {
+        return 0;
+    }
+    
+    public int getIdJob()
+    {
+        return 0;
+    }
+    
+    public String getDate()
+    {
+        return "oop";
+    }
+    
+    public int getTotalFee()
+    {
+        return 0;
+    }
+    
+    public void setId(int id) {}
+    public void setIdJob(int idJob) {}
+    public void setDate(String date) {}
+    public void setTotalFee(String totalFee) {}
+}
---
+++ Jobseeker.java
@@ -0,0 +1,62 @@
+
+/**
+ * Write a description of class Jobseeker here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Jobseeker
+{
+    // instance variables - replace the example below with your own
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String joinDate;
+
+    /**
+     * Constructor for objects of class Jobseeker
+     */
+    public Jobseeker()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getId()
+    {
+        return 0;
+    }
+    
+    public String getName()
+    {
+        return "oop";
+    }
+    
+    public String getEmail()
+    {
+        return "oop";
+    }
+    
+    public String getPassword()
+    {
+        return "oop";
+    }
+    
+    public String getJoinDate()
+    {
+        return "oop";
+    }
+    
+    public void setId(int id) {}
+    public void setName(String name) {}
+    public void setEmail(String email) {}
+    public void setPassword(String password) {}
+    public void setJoinDate(String joinDate) {}
+    
+}
